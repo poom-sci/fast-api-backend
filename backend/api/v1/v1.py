4bfb9b807414fa5bea5b861c020b8f556cc513ab
poom-such <poom.such@kkpfg.com>
2024-03-11 09:35:13 +0700

    Revert "Revert "add env""
    This reverts commit d5dcf19ce7fed79553f8e6f30003f2ddd099d64b.

--- backend/api/v1/v1.py
+++ backend/api/v1/v1.py
@@ -1,14 +1,19 @@
-
 from fastapi import APIRouter
 import http
 from core.service.transformer import predict
+import core.data_adapter.cache as cache
 
 router = APIRouter(tags=["v1"])
 
 # adminRouter.include_router(statusRouter, prefix="/status")
 
+
 # test endpoint
 @router.post("/test/{text}", status_code=http.HTTPStatus.OK)
-async def test( text: str):
-    res = predict(text)
-    return {"status": "ok", "message": "test", "req":text,"res": res}
+async def test(text: str):
+    if cache.is_cache_exists(text):
+        result = cache.get_cache_value(text)
+        return {"status": "ok", "message": "test", "req": text, "res": result}
+    prediction = predict(text)
+    cache.set_cache(text, prediction)
+    return {"status": "ok", "message": "test", "req": text, "res": prediction}
